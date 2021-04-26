7af651c45daabc40eb093b1c50882e56ed028db1
marcelladrian <marcell.dctr2@gmail.com>
2021-04-26 08:43:35 +0700

    menambahkan file simelet intellij (first commit)

---
+++ src/main/java/com/example/simelet_assignment_backend/SimeletAssignmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.simelet_assignment_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimeletAssignmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimeletAssignmentBackendApplication.class, args);
+    }
+
+}
